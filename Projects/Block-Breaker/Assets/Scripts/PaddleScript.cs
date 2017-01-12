0b91e93a68af363f31c8668403251847622eb7ea
jamesgibbons92 <jay.m.gibbons@gmail.com>
2017-01-12 21:18:47 +0000

    Blockbreaker

---
+++ Projects/Block-Breaker/Assets/Scripts/PaddleScript.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PaddleScript : MonoBehaviour {
+    public MusicPlayer music;
+
+    // public GameObject Paddle;
+    private float hits = 0.0f;
+	// Use this for initialization
+	void Start () {
+	
+	}
+
+    void OnCollisionEnter2D(Collision2D collision)
+    {
+        hits += 0.01f;
+        music.audio.pitch = hits;
+    }
+
+    // Update is called once per frame
+    void Update () {
+         //  print(Input.mousePosition);
+        Vector3 paddlePos = new Vector3 (0.0f,0.5f,0.0f);
+        //float paddlePos.x = Input.mousePosition.x;
+        //     Paddle.transform.Translate(Input.mousePosition);
+
+        paddlePos.x = Mathf.Clamp((Input.mousePosition.x * 0.02f),0.5f,15.5f);
+
+        
+        this.transform.position = paddlePos; //Input.mousePosition;
+	}
+}
